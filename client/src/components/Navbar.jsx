e9cc0d43f768f2b465854c61aac220869ccaca44
ayush patwal <137012714+ayushpatwal011@users.noreply.github.com>
2025-06-05 08:10:06 +0530

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -7,13 +7,13 @@ const Navbar = () => {
   return (
    <nav className="flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-3 sm:pb-3 sm:pt-6 border-b border-gray-300 ">
     <div className='flex gap-2 items-center text-2xl'>
-    <img src={assets.logo} alt="logo" className='w-8 sm:w-10 cursor-pointer' />
-     <strong className="pt-1">AyushDocs</strong>
+    <img src={assets.logo} alt="logo" className='w-6 sm:w-10 cursor-pointer' />
+     <strong className="pt-1 text-xs sm:text-lg">AyushDocs</strong>
     </div>
     <div>
       <button
       onClick={() => navigate("/admin")}
-       className="cursor-pointer flex justify-center items-center gap-2 px-6 py-1.5  transition rounded-3xl text-white bg-primary hover:bg-primary-dull">
+       className="cursor-pointer flex justify-center items-center gap-2 px-6 py-1.5 text-xs md:text-lg transition rounded-3xl text-white bg-primary hover:bg-primary-dull">
        {
         token ?  "Dashboard" : "Admin Login"
        }
